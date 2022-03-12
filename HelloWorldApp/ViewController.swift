b923abeb5b38e895a5977f89055ce3c6eb0ac765
denisbrk <buriakovdv@icloud.com>
2022-03-13 00:24:35 +0530

    Setup button for view Hello World

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden = true
+        greetingButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func greetingButtonPress() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle(greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting", for: .normal)
+        
+    }
 }
 
